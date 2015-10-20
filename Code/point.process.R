88f1068e144e50258ad02c25de21b980eb20293e
tylerascott <tyler.scott@uga.edu>
2015-10-20 12:54:58 -0700

    first remote commit

---
+++ Code/point.process.R
@@ -0,0 +1,42 @@
+rm(list=ls())
+require(foreign)
+require(plyr)
+require(dplyr)
+require(rgdal)
+require(sp)
+require(rgeos)
+require(maptools)
+require(ggplot2)
+require(reshape2)
+library(RcppRoll)
+library(devtools)
+library(RCurl)
+library(gdata)
+require(proj4)
+library(lubridate)
+require(RODBC)
+require(gridExtra)
+require(lattice)
+require(splancs)
+require(fields)
+library(raster)
+library(shapefiles)
+library(rasterVis)  # raster visualisation
+library(rWBclimate)
+library(stargazer)
+library(texreg)
+library(xtable)
+library(magrittr)
+library(ihs)
+
+library(spdep)
+library(INLA)
+
+
+counties = readOGR('Shapefiles','coastalcounties')
+
+nca.chem = read.csv('Input/nca_sedchemdata_narschallenge.csv')
+
+
+
+

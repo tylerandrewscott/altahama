a16265fe95b334f759b0f9ffb96c7a40077744d4
tylerascott <tyler.andrew.scott@gmail.com>
2015-10-27 22:34:20 -0400

    retry

--- Code/land.cover.R
+++ Code/land.cover.R
@@ -41,6 +41,7 @@ data.list = lapply(as.list(change.file.names),read.csv)
 change.df = join_all(data.list,type='full')
 rm(data.list)
 
+  
 uq.classes = union(unique(change.df$toClass),unique(change.df$fromClass))
 
 focal.change.df <- change.df %>% 
@@ -72,6 +73,7 @@ total.df = join_all(data.list,type='full')
 area.df = total.df %>% group_by(Year,FIPS,StateAbbrev,CountyName) %>% summarise(Area = sum(SquareMiles)) %>% dplyr::select(-Year)
 
 area.df <- area.df %>% data.frame(.) %>% dplyr::select(-Year) %>% mutate(uq = paste(FIPS,CountyName)) %>% filter(!duplicated(uq)) %>% dplyr::select(-uq)
+area.df$FIPS[nchar(as.character(area.df$FIPS))==4] = paste0(0,area.df$FIPS[nchar(as.character(area.df$FIPS))==4])
 
 focal.change.df <- join(focal.change.df,area.df)
 
@@ -203,16 +205,20 @@ focal.change.df$Part.Outreach.Active[is.na(focal.change.df$Part.Outreach)] <- 0
 
 focal.change.df$Area100sqm <- focal.change.df$Area/100
 
-focal.change.df$TotalChangePerYear = focal.change.df$TotalChange/(focal.change.df$ToYear-focal.change.df$FromYear)
+
 
 focal.change.df <- focal.change.df %>% filter(StateAbbrev!='DC') %>% mutate(FIPS = as.character(FIPS),ToPop10k = ToPop/10000,FromPop10k = FromPop/10000,state.gdp.pc.1k = state.gdp.pc/1000)
 
 us.county = readOGR(dsn = '../duckabush/','tl_2013_us_county')
 us.county = us.county[as.numeric(as.character(us.county$STATEFP))<=56,]
 us.county = us.county[as.numeric(as.character(us.county$STATEFP))%in% c(2,3,14,15)==FALSE,]
-us.county$CFIPS = as.numeric(as.character(paste0(us.county$STATEFP,us.county$COUNTYFP)))
-focal.change.df$CFIPS <- as.numeric(focal.change.df$FIPS)
+us.county$CFIPS = as.character(paste0(us.county$STATEFP,us.county$COUNTYFP))
+us.county$CFIPS[nchar(as.character(us.county$CFIPS))==4] = paste0(0,us.county$CFIPS[nchar(as.character(us.county$CFIPS))==4])
+focal.change.df$CFIPS <- as.character(focal.change.df$FIPS)
+
 us.county = us.county[us.county$CFIPS %in% focal.change.df$CFIPS,]
+
+
 
 row.names(us.county) <- as.character(seq(1,nrow(us.county)))
 us.county@data$rows <- row.names(us.county)
@@ -222,6 +228,42 @@ graph = inla.read.graph(filename='county.adj')
 county.ADJ = 'county.adj'
 
 focal.change.df$rowID <-  as.numeric(as.character(row.names(us.county)[match(focal.change.df$CFIPS, us.county@data$CFIPS)]))
+focal.change.df$PercentChange = focal.change.df$TotalChange/focal.change.df$Area
+
+
+
+bea.mw = read.csv('Input/bea_mw_income.csv')
+bea.atl = read.csv('Input/bea_atl_income.csv')
+bea.tex = read.csv('Input/bea_tex_inc.csv')
+bea.west = read.csv('Input/bea_west_income.csv')
+bea.gulf = read.csv('Input/bea_gulf_income.csv')
+bea.ne = read.csv('Input/bea_ne_income.csv')
+bea.va = read.csv('Input/bea_va_inc.csv')
+bea = join_all(list(bea.mw,bea.atl,bea.tex,bea.west,bea.ne,bea.va,bea.gulf),type='full')
+
+beal = gather(bea,Year,Value,-GeoFips,-GeoName,-LineCode,-Description)
+beal$Year = gsub('X','',beal$Year)
+beal = beal[grep('capita',beal$Description),]
+beal$GeoFips[nchar(beal$GeoFips)==4] = paste0(0,beal$GeoFips[nchar(beal$GeoFips)==4])
+beal$uq = paste(beal$GeoFips,beal$Year)
+focal.change.df$From.County.Per.Capita = beal$Value[match(paste(focal.change.df$CFIPS,focal.change.df$FromYear),beal$uq)]
+focal.change.df$To.County.Per.Capita = beal$Value[match(paste(focal.change.df$CFIPS,focal.change.df$ToYear),beal$uq)]
+
+sort(focal.change.df$CFIPS[is.na(focal.change.df$From.County.Per.Capita)])
+
+unique(beal$GeoFips[as.numeric(beal$GeoFips)<=51830&as.numeric(beal$GeoFips>=51059)])
+
+  beal$GeoFips[grep('Spots',beal$GeoName)]
+
+focal.change.df$From.County.Per.Capita = NA
+for (i in 1:nrow(focal.change.df))
+{
+
+  
+  
+}
+
+
 
 
 #model settings
@@ -232,7 +274,7 @@ WAIC = TRUE
 CPO = TRUE
 ### Land Cover Chqnge Models
 
-form.full.lc <- TotalChange ~ 1 + Perc.Change.Pop + Perc.Change.State.GDP + state.gdp.pc.1k + Area100sqm + FromPop10k + 
+form.full.lc <- TotalChangePerYear ~ 1 + Perc.Change.Pop + Perc.Change.State.GDP + state.gdp.pc.1k + Area100sqm + FromPop10k + 
   as.factor(Full.Approval.Active) + f(ToYear,model = 'iid') + 
   f(rowID,model='bym',graph = county.ADJ)
 
@@ -244,7 +286,7 @@ mod.full.lc <- inla(form.full.lc,family='gaussian',data = focal.change.df,
                       correct = TRUE,
                       correct.factor = correctionfactor))
 
-form.cond.lc <- TotalChange ~ 1 + Perc.Change.Pop + Perc.Change.State.GDP + state.gdp.pc.1k + Area100sqm + FromPop10k + 
+form.cond.lc <- TotalChangePerYear ~ 1 + Perc.Change.Pop + Perc.Change.State.GDP + state.gdp.pc.1k + Area100sqm + FromPop10k + 
   as.factor(Cond.Approval.Active) + f(ToYear,model = 'iid') + 
   f(rowID,model='bym',graph = county.ADJ)
 
@@ -256,7 +298,7 @@ mod.cond.lc <- inla(form.cond.lc,family='gaussian',data = focal.change.df,
                       correct = TRUE,
                       correct.factor = correctionfactor))
 
-form.both.lc <- TotalChange ~ 1 + Perc.Change.Pop + Perc.Change.State.GDP + state.gdp.pc.1k + Area100sqm + FromPop10k + 
+form.both.lc <- TotalChangePerYear ~ 1 + Perc.Change.Pop + Perc.Change.State.GDP + state.gdp.pc.1k + Area100sqm + FromPop10k + 
   as.factor(Cond.Approval.Active) + as.factor(Full.Approval.Active) + f(ToYear,model = 'iid') + 
   f(rowID,model='bym',graph = county.ADJ)
 
@@ -268,7 +310,7 @@ mod.both.lc <- inla(form.both.lc,family='gaussian',data = focal.change.df,
                       correct = TRUE,
                       correct.factor = correctionfactor))
 
-form.collab.coord.lc <- TotalChange ~ 1 + Perc.Change.Pop + Perc.Change.State.GDP + state.gdp.pc.1k + Area100sqm + FromPop10k + 
+form.collab.coord.lc <- TotalChangePerYear ~ 1 + Perc.Change.Pop + Perc.Change.State.GDP + state.gdp.pc.1k + Area100sqm + FromPop10k + 
   as.factor(Full.Approval.Active) + 
   as.factor(Coord.Formal.Agreements.Active) + 
   as.factor(Coord.Spec.Responsibilities.Active) + 
@@ -289,7 +331,7 @@ mod.collab.coord.lc <- inla(form.collab.coord.lc,family='gaussian',data = focal.
                         correct = TRUE,
                         correct.factor = correctionfactor))
 
-form.collab.part.lc <- TotalChange ~ 1 + Perc.Change.Pop + Perc.Change.State.GDP + state.gdp.pc.1k + Area100sqm + FromPop10k + 
+form.collab.part.lc <- TotalChangePerYear ~ 1 + Perc.Change.Pop + Perc.Change.State.GDP + state.gdp.pc.1k + Area100sqm + FromPop10k + 
   as.factor(Full.Approval.Active) + 
   as.factor(Part.Outreach.Active)+
   as.factor(Part.Advisory.Active)+
@@ -379,5 +421,7 @@ if(onscreen)
           custom.model.names = c('Coordination','Participation'),
           file = 'Output/Version1/lc.collab.table.html')}
 
+
+summary(mod.both.lc)
 
 
